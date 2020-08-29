9653f2ae58d54542e2385ff2facfa912d5a468a7
rafabruno <brunor_morais@hotmail.com>
2020-08-28 22:11:12 -0300

    adjust App.js

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -13,7 +13,7 @@ const App: React.FC = () => {
       <CssBaseline />
       <GlobalStyle />
       <SWRConfig value={swrConfig}>
-        <div />
+        <div>Hello World</div>
       </SWRConfig>
     </Provider>
   )
